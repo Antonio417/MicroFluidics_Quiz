9c4303f07a8f3140037500a7cbad7ad267885bd3
Antonio <airantoniofernando@gmail.com>
2023-02-27 22:23:11 +1100

    Changed Path

--- js/questions.js
+++ js/questions.js
@@ -3,91 +3,91 @@ let questions = [
     {
     numb: 1,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/easy1.jpeg",
+    answer: "Images/Normal/easy1.jpeg",
     options: [
-      "../mfdQuiz/Images/Normal/easy1.jpeg",
-      "../mfdQuiz/Images/Abnormal/easy1.jpg"
+      "Images/Normal/easy1.jpeg",
+      "Images/Abnormal/easy1.jpg"
     ]
   },
     {
     numb: 2,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/easy2.jpg",
+    answer: "Images/Normal/easy2.jpg",
     options: [
-      "../mfdQuiz/Images/Abnormal/easy2.jpg",
-      "../mfdQuiz/Images/Normal/easy2.jpg"
+      "Images/Abnormal/easy2.jpg",
+      "Images/Normal/easy2.jpg"
     ]
   },
     {
     numb: 3,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/easy3.jpg",
+    answer: "Images/Normal/easy3.jpg",
     options: [
-      "../mfdQuiz/Images/Abnormal/easy3.jpg",
-      "../mfdQuiz/Images/Normal/easy3.jpg"
+      "Images/Abnormal/easy3.jpg",
+      "Images/Normal/easy3.jpg"
     ]
   },
     {
     numb: 4,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/easy4.jpg",
+    answer: "Images/Normal/easy4.jpg",
     options: [
-      "../mfdQuiz/Images/Normal/easy4.jpg",
-      "../mfdQuiz/Images/Abnormal/easy4.jpg"
+      "Images/Normal/easy4.jpg",
+      "Images/Abnormal/easy4.jpg"
     ]
   },
     {
     numb: 5,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/easy5.jpg",
+    answer: "Images/Normal/easy5.jpg",
     options: [
-      "../mfdQuiz/Images/Normal/easy5.jpg",
-      "../mfdQuiz/Images/Abnormal/easy5.jpg"
+      "Images/Normal/easy5.jpg",
+      "Images/Abnormal/easy5.jpg"
     ]
   },
     {
     numb: 6,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/easy6.jpg",
+    answer: "Images/Normal/easy6.jpg",
     options: [
-      "../mfdQuiz/Images/Abnormal/easy6.jpg",
-      "../mfdQuiz/Images/Normal/easy6.jpg"
+      "Images/Abnormal/easy6.jpg",
+      "Images/Normal/easy6.jpg"
     ]
   },
     {
     numb: 7,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/hard1.jpg",
+    answer: "Images/Normal/hard1.jpg",
     options: [
-      "../mfdQuiz/Images/Normal/hard1.jpg",
-      "../mfdQuiz/Images/Abnormal/hard1.jpg"
+      "Images/Normal/hard1.jpg",
+      "Images/Abnormal/hard1.jpg"
     ]
   },
     {
     numb: 8,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/hard2.jpg",
+    answer: "Images/Normal/hard2.jpg",
     options: [
-      "../mfdQuiz/Images/Normal/hard2.jpg",
-      "../mfdQuiz/Images/Abnormal/hard2.jpg"
+      "Images/Normal/hard2.jpg",
+      "Images/Abnormal/hard2.jpg"
     ]
   },
     {
     numb: 9,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/hard3.jpg",
+    answer: "Images/Normal/hard3.jpg",
     options: [
-      "../mfdQuiz/Images/Normal/hard3.jpg",
-      "../mfdQuiz/Images/Abnormal/hard3.jpg"
+      "Images/Normal/hard3.jpg",
+      "Images/Abnormal/hard3.jpg"
     ]
   },
     {
     numb: 10,
     question: "Which sperm is normal?",
-    answer: "../mfdQuiz/Images/Normal/hard4.jpg",
+    answer: "Images/Normal/hard4.jpg",
     options: [
-      "../mfdQuiz/Images/Abnormal/hard4.jpg",
-      "../mfdQuiz/Images/Normal/hard4.jpg"
+      "Images/Abnormal/hard4.jpg",
+      "Images/Normal/hard4.jpg"
     ]
   }
 ];
