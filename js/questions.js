1072d3f3a1e5e9fba3a0a1e6c2c33fad9dfdbcb0
Antonio <airantoniofernando@gmail.com>
2023-03-01 01:08:29 +1100

    Changed Path Names

--- js/questions.js
+++ js/questions.js
@@ -4,91 +4,91 @@ let questions = [
     {
     numb: 1,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Easy/easy1.jpeg",
+    answer: "Images/Normal/Easy/easy1.jpeg",
     options: [
-      "Images2/Normal/Easy/easy1.jpeg",
-      "Images2/Abnormal/Easy/easy1.jpg"
+      "Images/Normal/Easy/easy1.jpeg",
+      "Images/Abnormal/Easy/easy1.jpg"
     ]
   },
     {
     numb: 2,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Easy/easy2.jpg",
+    answer: "Images/Normal/Easy/easy2.jpg",
     options: [
-      "Images2/Abnormal/Easy/easy2.jpg",
-      "Images2/Normal/Easy/easy2.jpg"
+      "Images/Abnormal/Easy/easy2.jpg",
+      "Images/Normal/Easy/easy2.jpg"
     ]
   },
     {
     numb: 3,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Easy/easy3.jpg",
+    answer: "Images/Normal/Easy/easy3.jpg",
     options: [
-      "Images2/Abnormal/Easy/easy3.jpg",
-      "Images2/Normal/Easy/easy3.jpg"
+      "Images/Abnormal/Easy/easy3.jpg",
+      "Images/Normal/Easy/easy3.jpg"
     ]
   },
     {
     numb: 4,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Easy/easy4.jpg",
+    answer: "Images/Normal/Easy/easy4.jpg",
     options: [
-      "Images2/Normal/Easy/easy4.jpg",
-      "Images2/Abnormal/Easy/easy4.jpg"
+      "Images/Normal/Easy/easy4.jpg",
+      "Images/Abnormal/Easy/easy4.jpg"
     ]
   },
     {
     numb: 5,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Easy/easy5.jpg",
+    answer: "Images/Normal/Easy/easy5.jpg",
     options: [
-      "Images2/Normal/Easy/easy5.jpg",
-      "Images2/Abnormal/Easy/easy5.jpg"
+      "Images/Normal/Easy/easy5.jpg",
+      "Images/Abnormal/Easy/easy5.jpg"
     ]
   },
     {
     numb: 6,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Easy/easy6.jpg",
+    answer: "Images/Normal/Easy/easy6.jpg",
     options: [
-      "Images2/Abnormal/Easy/easy6.jpg",
-      "Images2/Normal/Easy/easy6.jpg"
+      "Images/Abnormal/Easy/easy6.jpg",
+      "Images/Normal/Easy/easy6.jpg"
     ]
   },
     {
     numb: 7,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Hard/hard1.jpg",
+    answer: "Images/Normal/Hard/hard1.jpg",
     options: [
-      "Images2/Normal/Hard/hard1.jpg",
-      "Images2/Abnormal/Hard/hard1.jpg"
+      "Images/Normal/Hard/hard1.jpg",
+      "Images/Abnormal/Hard/hard1.jpg"
     ]
   },
     {
     numb: 8,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Hard/hard2.jpg",
+    answer: "Images/Normal/Hard/hard2.jpg",
     options: [
-      "Images2/Normal/Hard/hard2.jpg",
-      "Images2/Abnormal/Hard/hard2.jpg"
+      "Images/Normal/Hard/hard2.jpg",
+      "Images/Abnormal/Hard/hard2.jpg"
     ]
   },
     {
     numb: 9,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Hard/hard3.jpg",
+    answer: "Images/Normal/Hard/hard3.jpg",
     options: [
-      "Images2/Normal/Hard/hard3.jpg",
-      "Images2/Abnormal/Hard/hard3.jpg"
+      "Images/Normal/Hard/hard3.jpg",
+      "Images/Abnormal/Hard/hard3.jpg"
     ]
   },
     {
     numb: 10,
     question: "Which sperm is normal?",
-    answer: "Images2/Normal/Hard/hard4.jpg",
+    answer: "Images/Normal/Hard/hard4.jpg",
     options: [
-      "Images2/Abnormal/Hard/hard4.jpg",
-      "Images2/Normal/Hard/hard4.jpg"
+      "Images/Abnormal/Hard/hard4.jpg",
+      "Images/Normal/Hard/hard4.jpg"
     ]
   }
 ];
